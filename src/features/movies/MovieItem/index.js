3d03a97b99188dcaaa0b3a7975cc02c8500ef6cf
Joanna Górniak <joanna.gorniak.00@gmail.com>
2025-04-15 13:11:40 +0200

    created MovieItem component

---
+++ src/features/movies/MovieItem/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import {
+  MovieCard,
+  MovieImageWrapper,
+  MovieImage,
+  MovieTitle,
+  MovieYear,
+  MovieGenres,
+  GenreTag,
+  MovieRating,
+  RatingValue,
+  StarIconStyled,
+  Votes
+} from './styled';
+
+const MovieItem = ({ image, title, year, genres, rating, votes }) => {
+  return (
+    <MovieCard>
+      <MovieImageWrapper>
+        <MovieImage src={image} alt={title} />
+      </MovieImageWrapper>
+      <MovieTitle>{title}</MovieTitle>
+      <MovieYear>{year}</MovieYear>
+      <MovieGenres>
+        {genres.map((genre, index) => (
+          <GenreTag key={index}>{genre}</GenreTag>
+        ))}
+      </MovieGenres>
+      <MovieRating>
+        <StarIconStyled />
+        <RatingValue>{rating}</RatingValue>
+        <Votes>{votes} votes</Votes>
+      </MovieRating>
+    </MovieCard>
+  );
+};
+
+export default MovieItem;
