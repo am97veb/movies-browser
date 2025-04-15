a6d1e9c467fa77e623cfc40c14ba7f16eff0d9c4
Joanna Górniak <joanna.gorniak.00@gmail.com>
2025-04-15 13:12:59 +0200

    Set up App component

--- src/core/App/App.js
+++ src/core/App/App.js
@@ -1,11 +1,24 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { theme } from '../../theme';
+import MovieItem from '../../features/movies/MovieItem';
 
-
-function App() {
+const App = () => {
   return (
-    <div>
-     
-    </div>
+    <ThemeProvider theme={theme}>
+      <div>
+        <MovieItem
+          image="https://image.tmdb.org/t/p/w500/aKx1ARwG55zZ0GpRvU2WrGrCG9o.jpg"
+          title="Mulan"
+          year="2020"
+          genres={["Action", "Adventure", "Drama"]}
+          rating="7.1"
+          votes="5000"
+        />
+      </div>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
+
