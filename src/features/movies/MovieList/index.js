2558a7f071e1ab6475875294ce2b42c3c8c32941
Joanna Górniak <joanna.gorniak.00@gmail.com>
2025-04-16 18:43:15 +0200

    MovieList component created

---
+++ src/features/movies/MovieList/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import MovieItem from "../MovieItem";
+import { MovieListWrapper, MovieListHeading, MovieListGrid } from "./styled";
+
+
+
+const MovieList = ({ movies }) => {
+  return (
+    <MovieListWrapper>
+      <MovieListHeading>Popular movies</MovieListHeading>
+      <MovieListGrid>
+        {movies.map((movie) => (
+          <MovieItem
+            key={movie.id}
+            image={movie.image}
+            title={movie.title}
+            year={movie.year}
+            genres={movie.genres}
+            rating={movie.rating}
+            votes={movie.votes}
+          />
+        ))}
+      </MovieListGrid>
+    </MovieListWrapper>
+  );
+};
+
+export default MovieList;
