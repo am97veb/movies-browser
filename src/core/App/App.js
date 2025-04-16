e557be1e6994cf152c726d8a8ae362a8ea80411a
Joanna Górniak <joanna.gorniak.00@gmail.com>
2025-04-16 18:40:29 +0200

    App modified

--- src/core/App/App.js
+++ src/core/App/App.js
@@ -1,41 +1,22 @@
-<<<<<<< HEAD
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { theme } from '../../theme';
-import MovieItem from '../../features/movies/MovieItem';
-
-const App = () => {
-  return (
-    <ThemeProvider theme={theme}>
-      <div>
-        <MovieItem
-          image="https://image.tmdb.org/t/p/w500/aKx1ARwG55zZ0GpRvU2WrGrCG9o.jpg"
-          title="Mulan"
-          year="2020"
-          genres={["Action", "Adventure", "Drama"]}
-          rating="7.1"
-          votes="5000"
-        />
-      </div>
-    </ThemeProvider>
-  );
-};
-
-export default App;
-=======
 import { Navigation } from "../../common/Navigation";
 import { HashRouter } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
 import { MoviePage } from "../../features/movies/MoviePage";
 
-export default () => (
-  <HashRouter>
-    <Navigation />
-    <Routes>
-      <Route path="/movie" element={<MoviePage />}>
-      </Route>
-    </Routes>
-  </HashRouter>
-);
->>>>>>> main
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <HashRouter>
+        <Navigation />
+        <Routes>
+          <Route path="/movie" element={<MoviePage />} />
+        </Routes>
+      </HashRouter>
+    </ThemeProvider>
+  );
+}
 
+export default App;
