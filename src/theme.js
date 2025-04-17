ba8f1661ff7432916f95091fed607e708f5ce218
Adrian Markefka <am97veb@gmail.com>
2025-04-17 14:40:24 +0200

    breakpoints added

--- src/theme.js
+++ src/theme.js
@@ -3,6 +3,11 @@ export const theme = {
 
     },
     breakpoints: {
-
+        extraSmall: 320,
+        small: 576,
+        medium: 768,
+        large: 992,
+        extraLarge: 1200,
+        extraExtraLarge: 1400,
     },
 };
