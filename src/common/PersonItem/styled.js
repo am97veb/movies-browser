8d6929de2f42f56c979845ebf1ddc442f75533d8
Adrian Markefka <am97veb@gmail.com>
2025-04-17 14:45:40 +0200

    added object-fit to Picture, text-align to Name and media queries to all elements

--- src/common/PersonItem/styled.js
+++ src/common/PersonItem/styled.js
@@ -5,21 +5,51 @@ export const StyledPersonItem = styled.li`
     flex-direction: column;
     align-items: center;
     background-color: white;
-    width: 208px;
-    height: 339px;
+    max-width: 208px;
     border-radius: 5px;
-    padding: 16px ;
+    padding: 16px 16px 22px;
+
+    @media (max-width: ${({theme}) => theme.breakpoints.extraSmall}px) {
+        width: 136px;
+        height: 245px; 
+    }
 `;
 
 export const Picture = styled.img`
-    width: 176px;
-    height: 231px;
+    width: 100%;
     border-radius: 5px;
     margin-bottom: 12px;
+    object-fit: cover;
+
+    @media (max-width: ${({theme}) => theme.breakpoints.extraSmall}px) {
+        width: 120px;
+        height: 178px;
+        margin-bottom: 8px;
+    }
 `;
 
 export const Name = styled.p`
     font-size: 22px;
-    align-items: center;
+    text-align: center;
     margin: 0px;
+
+    @media (max-width: ${({theme}) => theme.breakpoints.extraLarge}px) {       
+        font-size: 2vw;
+    }
+
+    @media (max-width: ${({theme}) => theme.breakpoints.large}px) {       
+        font-size: 2vw;
+    }
+
+    @media (max-width: ${({theme}) => theme.breakpoints.medium}px) {
+       font-size: 2.5vw;
+    }
+
+    @media (max-width: ${({theme}) => theme.breakpoints.small}px) {
+       font-size: 3vw;
+    }
+
+    @media (max-width: ${({theme}) => theme.breakpoints.extraSmall}px) {
+        font-size: 14px;
+    }
 `;
