c66b16b8f12a93e8555023730c4e52e3ef12c453
Adrian Markefka <am97veb@gmail.com>
2025-04-17 14:48:50 +0200

    PopularPeople added

--- src/core/App/App.js
+++ src/core/App/App.js
@@ -2,12 +2,15 @@ import { Navigation } from "../../common/Navigation";
 import { HashRouter } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
 import { MoviePage } from "../../features/movies/MoviePage";
+import { PopularPeople } from "../../features/people/PopularPeople";
 
 export default () => (
   <HashRouter>
     <Navigation />
     <Routes>
       <Route path="/movie" element={<MoviePage />}>
+      </Route>
+      <Route path="/people" element={<PopularPeople />}>
       </Route>
     </Routes>
   </HashRouter>
