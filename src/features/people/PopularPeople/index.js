8333f970bfe8db11111dc68e031f2d30caa3bf59
Adrian Markefka <am97veb@gmail.com>
2025-04-18 15:15:39 +0200

    map added

--- src/features/people/PopularPeople/index.js
+++ src/features/people/PopularPeople/index.js
@@ -1,14 +1,23 @@
+import { useSelector } from "react-redux";
 import { PersonItem } from "../../../common/PersonItem/index";
 import { Wrapper, Header, PeopleList } from "./styled";
+import { selectPeople } from "../peopleSlice";
 
+export const PopularPeople = () => {
+    const people = useSelector(selectPeople);
 
-export const PopularPeople = () => (
-    <Wrapper>
-        <Header>
-            Popular people
-        </Header>
-        <PeopleList>
-            <PersonItem/>
-        </PeopleList>
-    </Wrapper>
-)
+    return (
+        <Wrapper>
+            <Header>
+                Popular people
+            </Header>
+            <PeopleList>
+                {people.map(person =>
+                    <PersonItem
+                        person={person}
+                    />
+                )}
+            </PeopleList>
+        </Wrapper>
+    )
+}
