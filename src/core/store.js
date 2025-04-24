729022fb37d1d838b33377f6c4fd31ca39112bec
Adrian Markefka <am97veb@gmail.com>
2025-04-24 12:23:12 +0200

    moviesSaga and peopleSaga to rootSaga added

--- src/core/store.js
+++ src/core/store.js
@@ -1,17 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
 import peopleReducer from "../features/people/peopleSlice";
-import {peopleSaga} from "../features/people/peopleSaga";
+import { peopleSaga } from "../features/people/peopleSaga";
+import moviesReducer from "../features/movies/moviesSlice";
 import createSagaMiddleware from "redux-saga";
+import rootSaga from "./saga";
 
 const sagaMiddleware = createSagaMiddleware();
 
 export default configureStore({
     reducer: {
         people: peopleReducer,
+        movies: moviesReducer,
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(sagaMiddleware),
-      
+
 });
 
-sagaMiddleware.run(peopleSaga);
+sagaMiddleware.run(rootSaga);
