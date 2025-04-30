d83d1af071a59aa0b71ef7066ba24c6a79179c95
Joanna Górniak <joanna.gorniak.00@gmail.com>
2025-04-30 15:07:03 +0200

    NoResult component created

---
+++ src/common/Navigation/Search/NoResult/styled.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const Message = styled.h2`
+  
+  font-weight: 600;
+  font-size: 36px;
+  line-height: 1.2;
+  text-align: center;
+  color: #000;
+  max-width: 674px;
+  margin: 150px auto 0 auto; 
+`;
+
+export const IconWrapper = styled.div`
+  margin: 50px auto 0 auto;
+  max-width: 668px;
+  width: 100%;
+
+  svg {
+    width: 100%;
+    height: auto;
+    display: block;
+  }
+`;
