dc34b0e46b2dab2ad44c7e9082545d844fd8376f
Joanna Górniak <joanna.gorniak.00@gmail.com>
2025-05-05 17:25:39 +0200

    Update App with new route configuration

--- src/core/App/App.js
+++ src/core/App/App.js
@@ -1,20 +1,20 @@
-import { Navigation } from "../../common/Navigation";
 import { HashRouter } from "react-router-dom";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Navigation } from "../../common/Navigation";
 import { MoviePage } from "../../features/movies/MoviePage";
 import { PopularPeople } from "../../features/people/PopularPeople";
 import MovieList from "../../features/movies/MovieList";
-import { movies } from "../../common/Navigation/Search/SearchResult/moviesData"; 
 
-export default () => (
-  <HashRouter>
-    <Navigation />
-    <Routes>
-    <Route path="/movies" element={<MovieList movies={movies} />} />
-    <Route path="/movies/:id" element={<MoviePage />} />
-<Route path="/people" element={<PopularPeople />} />
-      
-    </Routes>
-  </HashRouter>
-);
-
+export default function App() {
+  return (
+    <HashRouter>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Navigate to="/movies" />} />
+        <Route path="/movies" element={<MovieList />} />
+        <Route path="/movies/:id" element={<MoviePage />} />
+        <Route path="/people" element={<PopularPeople />} />
+      </Routes>
+    </HashRouter>
+  );
+}
