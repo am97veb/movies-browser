3f8d84058ffaa56250413760a99567ff11252635
Adrian Markefka <am97veb@gmail.com>
2025-05-05 12:04:36 +0200

    added actions to takeEvery and modified api link based on page number

--- src/features/people/peopleSaga.js
+++ src/features/people/peopleSaga.js
@@ -1,16 +1,27 @@
-import { takeEvery, put, call } from "redux-saga/effects";
-import { showPeople, fetchDataError, fetchDataSuccess } from "./peopleSlice";
-import { fetchApiData } from "../../fetchApiData";
+import { takeEvery, put, call, select } from "redux-saga/effects";
+import { showPeople, fetchDataError, fetchDataSuccess, selectPage, nextPage, previousPage, firstPage, lastPage } from "./peopleSlice";
+import { fetchPeopleData } from "../../fetchPeopleData";
 
 function* fetchPeopleHandler() {
     try {
-        const peopleData = yield call(fetchApiData);
-        yield put(fetchDataSuccess(peopleData));
+        const page = yield select(selectPage);
+        const sourceApiData = `moviesSource${page}.json`;
+        const people = yield call(fetchPeopleData, sourceApiData);
+        yield put(fetchDataSuccess(people));
     } catch (error) {
         yield put(fetchDataError());
     }
 }
 
 export function* peopleSaga() {
-    yield takeEvery(showPeople.type, fetchPeopleHandler);
+    yield takeEvery(
+        [
+            showPeople.type,
+            firstPage.type,
+            previousPage.type,
+            nextPage.type,
+            lastPage.type,
+        ],
+        fetchPeopleHandler
+    );
 }
