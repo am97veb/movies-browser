30d6a3778de17cd455f53bc056ddd15c6e2d8959
Adrian Markefka <am97veb@gmail.com>
2025-05-05 11:59:27 +0200

    added properties to initialState and reducers

--- src/features/people/peopleSlice.js
+++ src/features/people/peopleSlice.js
@@ -4,20 +4,38 @@ const peopleSlice = createSlice({
     name: "people",
     initialState: {
         peopleData: [],
+        page: 1,
+        totalPages: 1,
         fetchStatus: "loading",
     },
     reducers: {
         showPeople: () => { },
-        fetchDataSuccess: (state, { payload: peopleData }) => {
+        fetchDataSuccess: (state, { payload: people }) => {
             state.fetchStatus = "success";
-            state.peopleData = peopleData;
+            state.peopleData = people;
+            state.totalPages = people.total_pages;
         },
         fetchDataError: (state) => {
             state.fetchStatus = "error";
         },
+        firstPage:(state) => {
+            state.page = 1
+        },
+        nextPage:(state) => {
+            state.page++;  
+        },
+        previousPage: (state) => {
+            state.page--;
+        },
+        lastPage: (state) => {
+            state.page = state.totalPages
+        }
     },
 });
 
-export const { showPeople, fetchDataSuccess, fetchDataError } = peopleSlice.actions;
+export const { showPeople, fetchDataSuccess, fetchDataError, firstPage, nextPage, previousPage, lastPage } = peopleSlice.actions;
 export const selectPeople = state => state.people.peopleData;
+export const selectPage = state => state.people.page;
+export const selectPagesNumber = state => state.people.pagesNumber;
+export const selectTotalPages = state => state.people.totalPages;
 export default peopleSlice.reducer;
