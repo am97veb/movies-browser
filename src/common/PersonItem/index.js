f9e689d9824a31ceaa82e51f07bb33edd51e016d
Adrian Markefka <am97veb@gmail.com>
2025-05-06 14:58:05 +0200

    added condition and deleted unnecessery spaces

--- src/common/PersonItem/index.js
+++ src/common/PersonItem/index.js
@@ -1,19 +1,18 @@
 import { StyledPersonItem, Picture, EmptyPoster, Name } from "./styled";
-import {ReactComponent as EmptyProfilePath} from "./emptyProfilePath.svg"
-
+import { ReactComponent as EmptyProfilePath } from "./emptyProfilePath.svg"
 
 export const PersonItem = ({ person }) => (
-
-
     <StyledPersonItem>
-        {person.profile_path ?
-        <Picture
-            src={`https://image.tmdb.org/t/p/w500${person.profile_path}`}>
-        </Picture>
-        :
-        <EmptyPoster>
-            <EmptyProfilePath/>
-        </EmptyPoster>
+        {person &&
+            (person.profile_path ?
+                <Picture
+                    src={`https://image.tmdb.org/t/p/w500${person.profile_path}`}>
+                </Picture>
+                :
+                <EmptyPoster>
+                    <EmptyProfilePath />
+                </EmptyPoster>
+            )
         }
         <Name>
             {person && person.name}
