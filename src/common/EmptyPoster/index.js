ce574793a8a70cc67bc853d678612fe9eb1c9a31
Adrian Markefka <am97veb@gmail.com>
2025-05-12 16:49:51 +0200

    added "float" attribute, thanks to which StyledEmptyPoster will change its properties

--- src/common/EmptyPoster/index.js
+++ src/common/EmptyPoster/index.js
@@ -1,12 +1,14 @@
 import { useSelector } from "react-redux"
 import { StyledEmptyPoster, StyledEmptyPosterPath, StyledEmptyProfilePath } from "./styled"
 import { selectContentType } from "../Pagination/paginationSlice"
+import { useLocation } from "react-router-dom";
 
 export const EmptyPoster = () => {
     const contentType = useSelector(selectContentType);
+    const location = useLocation();
 
     return (
-        <StyledEmptyPoster >
+        <StyledEmptyPoster float={location.pathname.includes("person") ? true : false} >
             {contentType === null || contentType === "movies"
                 ?
                 <StyledEmptyPosterPath />
