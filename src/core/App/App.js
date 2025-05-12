4f41f1f7c592ebf38f67e55e51bd336da28f635c
Adrian Markefka <am97veb@gmail.com>
2025-05-12 11:00:53 +0200

    PersonPage to Routes added

--- src/core/App/App.js
+++ src/core/App/App.js
@@ -5,6 +5,7 @@ import { MoviePage } from "../../features/movies/MoviePage";
 import { PopularPeople } from "../../features/people/PopularPeople";
 import MovieList from "../../features/movies/MovieList";
 import { movies } from "../../common/Navigation/Search/SearchResult/moviesData"; 
+import { PersonPage } from "../../features/people/PersonPage";
 
 export default () => (
   <HashRouter>
@@ -12,8 +13,8 @@ export default () => (
     <Routes>
     <Route path="/movies" element={<MovieList movies={movies} />} />
     <Route path="/movies/:id" element={<MoviePage />} />
-<Route path="/people" element={<PopularPeople />} />
-      
+    <Route path="/people" element={<PopularPeople />} />
+    <Route path="/people/:id" element={<PersonPage />} />
     </Routes>
   </HashRouter>
 );
