5311723f1f332b0cd18df95fd34dcdcce7157b63
Adrian Markefka <am97veb@gmail.com>
2025-05-12 10:58:41 +0200

    personSaga added

--- src/core/saga.js
+++ src/core/saga.js
@@ -2,11 +2,13 @@ import { all } from "redux-saga/effects";
 import { moviesSaga } from "../features/movies/moviesSaga";
 import { peopleSaga } from "../features/people/peopleSaga";
 import { paginationSaga } from "../common/Pagination/paginationSaga";
+import { personSaga } from "../features/people/PersonPage/personSaga";
 
 export default function* rootSaga() {
     yield all ([
         moviesSaga(),
         peopleSaga(),
         paginationSaga(),
+        personSaga(),
     ]);
 }
