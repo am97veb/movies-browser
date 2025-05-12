1112b5568991e121ad2a1abdaf70fb0ff682dda1
Adrian Markefka <am97veb@gmail.com>
2025-05-12 14:57:36 +0200

    People component added

--- src/core/App/App.js
+++ src/core/App/App.js
@@ -2,19 +2,20 @@ import { Navigation } from "../../common/Navigation";
 import { HashRouter } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
 import { MoviePage } from "../../features/movies/MoviePage";
-import { PopularPeople } from "../../features/people/PopularPeople";
 import MovieList from "../../features/movies/MovieList";
-import { movies } from "../../common/Navigation/Search/SearchResult/moviesData"; 
+import { movies } from "../../common/Navigation/Search/SearchResult/moviesData";
 import { PersonPage } from "../../features/people/PersonPage";
+import { People } from "../../features/people/People";
 
 export default () => (
   <HashRouter>
     <Navigation />
     <Routes>
-    <Route path="/movies" element={<MovieList movies={movies} />} />
-    <Route path="/movies/:id" element={<MoviePage />} />
-    <Route path="/people" element={<PopularPeople />} />
-    <Route path="/people/:id" element={<PersonPage />} />
+      <Route path="/movies" element={<MovieList movies={movies} />} />
+      <Route path="/movies/:id" element={<MoviePage />} />
+      <Route path="/people" element={<People />} />
+      <Route path="/people/:id" element={<PersonPage />} />
+      
     </Routes>
   </HashRouter>
 );
