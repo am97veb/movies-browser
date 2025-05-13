a83c245a689cefb68c2c25015a30ec4fe8945f61
Marcin Osojca <osojca.marcin1@gmail.com>
2025-05-13 12:14:20 +0200

    MoviePage component updated

--- src/features/movies/MoviePage/index.js
+++ src/features/movies/MoviePage/index.js
@@ -4,27 +4,46 @@ import { ListSection } from "./ListSection";
 import { PosterHeader } from "./PosterHeader";
 import { MoviePageSection } from "./PosterHeader/styled";
 import { PersonItem } from "../../../common/PersonItem";
-import { movies } from "../../../common/Navigation/Search/SearchResult/moviesData";
+import { useSelector } from "react-redux";
+import { selectMovies } from "../moviesSlice";
+import { selectGenres } from "../moviesSlice";
 
 export const MoviePage = () => {
     const { id } = useParams();
+    const movies = useSelector(selectMovies);
     const movie = movies.find(movie => movie.id === Number(id));
+    console.log(movie);
+    console.log(movie.release_date);
+    const genresList = useSelector(selectGenres);
 
+
+    const mapGenres = (genre_ids) => {
+        return genre_ids.map((id) => {
+            const genre = genresList.find((g) => g.id === id);
+            return genre ? genre.name : "Unknown";
+        });
+    };
 
 
     return (
         <>
-            <PosterHeader />
+            <PosterHeader
+                backdrop={movie.backdrop_path}
+                title={movie.title}
+                rating={movie.vote_average.toFixed(1)}
+                votes={movie.vote_count}
+            />
             <MoviePageSection>
                 <DescriptionTile
+                    poster={movie.poster_path}
                     title={movie.title}
-                    year={movie.year}
-                    poster={movie.image}
+                    year={movie.release_date.split("-")[0]}
                     production={movie.production}
-                    releaseDate={movie.releaseDate}
-                    genres={movie.genres}
-                    rating={movie.rating}
-                    description={movie.description}
+                    releaseDate={movie.release_date}
+                    genres={mapGenres(movie.genre_ids)}
+                    rating={movie.vote_average}
+                    votes={movie.vote_count}
+                    description={movie.overview}
                 />
                 <ListSection
                     headerContent={"Cast"}
