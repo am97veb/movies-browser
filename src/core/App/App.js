d374c9c5cf626c8bd36ba39b1cb3c10cfbed15f3
Marcin Osojca <osojca.marcin1@gmail.com>
2025-05-14 20:36:26 +0200

    App.js with Route actualized

--- src/core/App/App.js
+++ src/core/App/App.js
@@ -1,21 +1,22 @@
 import { Navigation } from "../../common/Navigation";
-import { HashRouter } from "react-router-dom";
-import { Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import { MoviePage } from "../../features/movies/MoviePage";
 import MovieList from "../../features/movies/MovieList";
 import { movies } from "../../common/Navigation/Search/SearchResult/moviesData";
 import { PersonPage } from "../../features/people/PersonPage";
 import { People } from "../../features/people/People";
+import { ScrollToTop } from "../../scrollToTop";
 
 export default () => (
   <HashRouter>
+    <ScrollToTop />
     <Navigation />
     <Routes>
       <Route path="/movies" element={<MovieList movies={movies} />} />
       <Route path="/movies/:id" element={<MoviePage />} />
       <Route path="/people" element={<People />} />
       <Route path="/people/person/:id" element={<PersonPage />} />
-      
+      <Route path="/" element={<Navigate to="/movies" />} />
     </Routes>
   </HashRouter>
 );
