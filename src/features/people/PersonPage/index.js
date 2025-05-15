b11abcf06452f0ca79dad8e24663c5b083528e3b
Adrian Markefka <am97veb@gmail.com>
2025-05-15 12:03:40 +0200

    added dependency to cast and crew list

--- src/features/people/PersonPage/index.js
+++ src/features/people/PersonPage/index.js
@@ -24,35 +24,39 @@ export const PersonPage = () => {
             <DescriptionTile
                 person={person}
             />
-            <ListSection
-                headerContent={"Movies - cast"}
-                length={cast && cast.length}
-                sectionContent={cast && cast.map(cast => (
-                    <MovieItem
-                        key={cast.id}
-                        image={`https://image.tmdb.org/t/p/w500${cast.poster_path}`}
-                        title={cast.original_title}
-                        character={cast.character}
-                        year={cast.release_date}
-                        genres={cast.genres}
-                        rating={cast.rating}
-                        votes={cast.vote_average} />
-                ))}
-            />
-            <ListSection
-                headerContent={"Movies - crew"}
-                length={crew && crew.length}
-                sectionContent={crew && crew.map(crew => (
-                    <MovieItem
-                        key={crew.id}
-                        image={`https://image.tmdb.org/t/p/w500${crew.poster_path}`}
-                        title={crew.original_title}
-                        year={crew.release_date}
-                        genres={crew.genres}
-                        rating={crew.rating}
-                        votes={crew.vote_average} />
-                ))}
-            />
+            {cast.length !== 0 &&
+                <ListSection
+                    headerContent={"Movies - cast"}
+                    length={cast.length}
+                    sectionContent={cast.map(cast => (
+                        <MovieItem
+                            key={cast.id}
+                            image={`https://image.tmdb.org/t/p/w500${cast.poster_path}`}
+                            title={cast.original_title}
+                            character={cast.character}
+                            year={cast.release_date}
+                            genres={cast.genres}
+                            rating={cast.rating}
+                            votes={cast.vote_average} />
+                    ))}
+                />
+            }
+            {crew.length !== 0 &&
+                <ListSection
+                    headerContent={"Movies - crew"}
+                    length={crew.length}
+                    sectionContent={crew.map(crew => (
+                        <MovieItem
+                            key={crew.id}
+                            image={`https://image.tmdb.org/t/p/w500${crew.poster_path}`}
+                            title={crew.original_title}
+                            year={crew.release_date}
+                            genres={crew.genres}
+                            rating={crew.rating}
+                            votes={crew.vote_average} />
+                    ))}
+                />
+            }
         </PersonPageSection>
     );
 };
