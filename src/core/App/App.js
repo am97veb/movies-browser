3d252aec6de86b6e2374b8bd730ff8653b35a606
Marcin Osojca <osojca.marcin1@gmail.com>
2025-05-15 20:20:24 +0200

    Movies component to App.js added

--- src/core/App/App.js
+++ src/core/App/App.js
@@ -2,17 +2,17 @@ import { Navigation } from "../../common/Navigation";
 import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import { MoviePage } from "../../features/movies/MoviePage";
 import MovieList from "../../features/movies/MovieList";
-import { movies } from "../../common/Navigation/Search/SearchResult/moviesData";
 import { PersonPage } from "../../features/people/PersonPage";
 import { People } from "../../features/people/People";
 import { ScrollToTop } from "../../scrollToTop";
+import { Movies } from "../../features/movies/Movies";
 
 export default () => (
   <HashRouter>
     <ScrollToTop />
     <Navigation />
     <Routes>
-      <Route path="/movies" element={<MovieList movies={movies} />} />
+      <Route path="/movies" element={<Movies />} />
       <Route path="/movies/:id" element={<MoviePage />} />
       <Route path="/people" element={<People />} />
       <Route path="/people/person/:id" element={<PersonPage />} />
