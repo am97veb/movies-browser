57a507e9fbc3a00063d341be50d6709c5cec6fff
Adrian Markefka <am97veb@gmail.com>
2025-05-20 13:24:11 +0200

    delay to longer Loader view added

--- src/features/people/peopleSaga.js
+++ src/features/people/peopleSaga.js
@@ -1,19 +1,23 @@
-import { takeEvery, put, call, select } from "redux-saga/effects";
+import { takeEvery, put, call, select, delay } from "redux-saga/effects";
 import { showPeople, fetchDataError, fetchDataSuccess } from "./peopleSlice";
 import { fetchApiData } from "../../fetchApiData";
 import { contentType, totalPages, selectPage, selectContentType } from "../../common/Pagination/paginationSlice";
 
 export function* fetchPeopleHandler() {
     try {
+
+        yield delay(5000);
         const type = yield select(selectContentType);
         if (type !== "people") {
             yield put(contentType("people"));
         };
         const page = yield select(selectPage);
         const sourceApiData = `https://api.themoviedb.org/3/person/popular?api_key=6007bf485fd1645cfc7ab81654ba3228&language=en-US&page=${page}`;
+        
         const people = yield call(fetchApiData, sourceApiData);
         yield put(fetchDataSuccess(people));
         yield put(totalPages(people));
+        console.log(people);
     } catch (error) {
         yield put(fetchDataError());
     }
