cee404412a0af725b92b8b5fd1dfb8cb411131d7
Marcin Osojca <osojca.marcin1@gmail.com>
2025-05-21 20:07:09 +0200

    PersonPageSection css adjusted

--- src/features/people/PersonPage/styled.js
+++ src/features/people/PersonPage/styled.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const PersonPageSection = styled.section`
-    max-width: 1368px;
+    max-width: 1400px;
     min-width: 300px;
-    margin: 0px auto;
+    margin: 0 auto;
 `;
