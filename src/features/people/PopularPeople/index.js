79950027658d33794f353a4c96e35f7b0a6e3609
Adrian Markefka <am97veb@gmail.com>
2025-05-22 16:42:11 +0200

    SwitchContent added

--- src/features/people/PopularPeople/index.js
+++ src/features/people/PopularPeople/index.js
@@ -8,48 +8,48 @@ import { useQueryParameters } from "../../../common/Navigation/Search/queryParam
 import searchQueryParamName from "../../../common/Navigation/Search/searchQueryParamName";
 import { selectSearchResult, selectQuery } from "../searchSlice";
 import { selectPage } from "../../../common/Pagination/paginationSlice";
-import Loading from "../../../common/Navigation/Search/Loading";
+import { SwitchContent } from "../../../common/SwitchContent";
 
 export const PopularPeople = () => {
-    const query = useQueryParameters(searchQueryParamName);
-    const sliceQuery = useSelector(selectQuery);
-    const page = useSelector(selectPage);
-    const fetchStatus = useSelector(selectPeopleStatus);
-    console.log(fetchStatus);
+   const query = useQueryParameters(searchQueryParamName);
+   const sliceQuery = useSelector(selectQuery);
+   const page = useSelector(selectPage);
+   const fetchStatus = useSelector(selectPeopleStatus);
 
+   const people = useSelector(state =>
+      (query?.trim() && query === sliceQuery)
+         ? selectSearchResult(state)
+         : selectPeople(state));
 
-    const people = useSelector(state =>
-        (query?.trim() && query === sliceQuery)
-            ? selectSearchResult(state)
-            : selectPeople(state));
+   useEffect(() => {
+      window.scrollTo(0, 0);
+   }, [page])
 
-    useEffect(() => {
-        window.scrollTo(0, 0);
-    }, [page])
-
-    return (
-        <Wrapper>
-            <Header>
-                Popular people
-            </Header>
-            {fetchStatus === "loading"
-                ?
-                <Loading />
-                :
-                <PeopleList>
-                    {Array.isArray(people) &&
+   return (
+      <Wrapper>
+         <Header>
+            Popular people
+         </Header>
+         <SwitchContent
+            status={fetchStatus}
+            content={
+               <>
+                  <PeopleList>
+                     {Array.isArray(people) &&
                         people.map(person => (
-                            <PersonItem
-                                key={person.id}
-                                id={person.id}
-                                image={person.profile_path}
-                                name={person.name}
-                            />
+                           <PersonItem
+                              key={person.id}
+                              id={person.id}
+                              image={person.profile_path}
+                              name={person.name}
+                           />
                         ))
-                    }
-                </PeopleList>
+                     }
+                  </PeopleList>
+                  <Pagination />
+               </>
             }
-            <Pagination />
-        </Wrapper>
-    )
+         />
+      </Wrapper>
+   )
 }
