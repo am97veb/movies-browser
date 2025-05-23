a342b240bc39ce226f434e88b17affdd3df0373b
Adrian Markefka <am97veb@gmail.com>
2025-05-23 17:04:15 +0200

    changed styles some elements and created AboutMovie

--- src/common/MovieItem/styled.js
+++ src/common/MovieItem/styled.js
@@ -5,31 +5,45 @@ import { ReactComponent as StarIcon } from './StarIcon.svg';
 export const MovieCard = styled.li`
   max-width: 324px;
   width: 100%;
-  height: 700px; 
+  height: 100%; 
   border-radius: 5px;
   padding: 16px;
   background-color: ${({ theme }) => theme.colors.white};
   box-shadow: 0px 4px 12px rgba(186, 199, 213, 0.5);
-  box-sizing: border-box;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
+ 
+    @media (max-width: ${({ theme }) => theme.breakpoints.extraSmall}px) {
+      height: 201px;
+      padding: 14px 8px 14px 14px;
+    }
 `;
 
 export const StyledLink = styled(Link)`
-  text-decoration: none;
+    text-decoration: none;
+    display: flex;
+    flex-direction: column;
+    gap: 14px;
+    height: 100%;
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.extraSmall}px) {
+      width: inherit;
+      flex-direction: row;    
+    }
 `;
 
 export const MovieImageWrapper = styled.div`
   width: 100%;
   height: 434px;
   border-radius: 5px;
-  overflow: hidden;
   align-self: center;
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: rgba(196, 196, 196, 1);
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.extraSmall}px) {
+      max-width: 114px;
+      height: 169px;
+      align-self: flex-start;
+    }
 `;
 
 export const MovieImage = styled.img`
@@ -37,6 +51,13 @@ export const MovieImage = styled.img`
   height: 100%;
   object-fit: cover;
   display: block;
+  border-radius: 5px;
+`;
+
+export const AboutMovie = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
 `;
 
 export const MovieTitle = styled.h3`
@@ -44,8 +65,12 @@ export const MovieTitle = styled.h3`
   font-size: 22px;
   line-height: 130%;
   color: ${({ theme }) => theme.colors.black};
-  margin: 16px 0 4px;
+  margin: 0px 0px 8px;
   text-align: left;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.extraSmall}px) {
+      font-size: 16px;
+      }
 `;
 
 export const MovieYear = styled.p`
@@ -53,20 +78,29 @@ export const MovieYear = styled.p`
   font-size: 16px;
   line-height: 150%;
   color: ${({ theme }) => theme.colors.lightSlateGrey};
-  margin: 0 0 12px;
+  margin: 0 0 8px;
   text-align: left;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.extraSmall}px) {
+       font-size: 13px;
+       line-height: 130%;
+      }
 `;
 
 export const MovieCharacter = styled(MovieYear)`
-
+  margin: 0px 0px 8px;
 `;
 
 export const MovieGenres = styled.div`
   display: flex;
-  gap: 10px;
-  margin: 0 0 16px;
+  gap: 8px;
   flex-wrap: wrap;
   justify-content: flex-start;
+  margin-bottom: 10px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.extraSmall}px) {
+        gap: 4px;
+      }
 `;
 
 export const GenreTag = styled.span`
@@ -78,13 +112,17 @@ export const GenreTag = styled.span`
   font-size: 14px;
   line-height: 140%;
   white-space: nowrap;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.extraSmall}px) {
+    font-size: 10px;
+    padding: 4px 8px;
+  }
 `;
 
 export const MovieRating = styled.div`
   display: flex;
-  align-items: center;
-  gap: 12px;
   margin-top: auto;
+  gap: 12px;
 `;
 
 export const RatingValue = styled.span`
@@ -93,17 +131,10 @@ export const RatingValue = styled.span`
   line-height: 150%;
   color: ${({ theme }) => theme.colors.black};
 
-        @media (max-width: ${({ theme }) => theme.breakpoints.medium}px) {
-            font-size: 13px;
-            line-height: 130%;
-        }
-                
-        @media (max-width: ${({ theme }) => theme.breakpoints.small}px) {
-            
-        }
-        @media (max-width: ${({ theme }) => theme.breakpoints.extraSmall}px) {
-            
-        }
+  @media (max-width: ${({ theme }) => theme.breakpoints.medium}px) {
+      font-size: 13px;
+      line-height: 130%;
+  }
 `;
 
 export const StarIconStyled = styled(StarIcon)`
@@ -111,16 +142,9 @@ export const StarIconStyled = styled(StarIcon)`
   height: auto;
   fill: ${({ theme }) => theme.colors.deepLemon};
 
-        @media (max-width: ${({ theme }) => theme.breakpoints.medium}px) {
-          width: 16px;
-        }
-                
-        @media (max-width: ${({ theme }) => theme.breakpoints.small}px) {
-            
-        }
-        @media (max-width: ${({ theme }) => theme.breakpoints.extraSmall}px) {
-            
-        }
+  @media (max-width: ${({ theme }) => theme.breakpoints.medium}px) {
+    width: 16px;
+  }
 `;
 
 export const Votes = styled.span`
@@ -130,14 +154,7 @@ export const Votes = styled.span`
   color: ${({ theme }) => theme.colors.lightSlateGrey};
 
   @media (max-width: ${({ theme }) => theme.breakpoints.medium}px) {
-            font-size: 13px;
-            line-height: 130%;
-        }
-                
-        @media (max-width: ${({ theme }) => theme.breakpoints.small}px) {
-            
-        }
-        @media (max-width: ${({ theme }) => theme.breakpoints.extraSmall}px) {
-            
-        }
+    font-size: 13px;
+    line-height: 130%;
+  }
 `;
