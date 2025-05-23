aa0e80b0f47061f5ac4de727770dd2a2092031b6
Adrian Markefka <am97veb@gmail.com>
2025-05-23 11:10:25 +0200

    dispatch fetchPersonLoading in fetchPersonHandler; added delay effect and deleted selectPersonId

--- src/features/people/PersonPage/personSaga.js
+++ src/features/people/PersonPage/personSaga.js
@@ -1,33 +1,31 @@
-import { takeEvery, call, put, select } from "redux-saga/effects";
-import { showPerson, fetchPersonSuccess, fetchPersonError, fetchFilmographySuccess, fetchFilmographyError  } from "./personSlice";
+import { takeEvery, call, put, delay } from "redux-saga/effects";
+import { showPerson, fetchPersonSuccess,fetchPersonLoading, fetchPersonError, fetchFilmographySuccess, fetchFilmographyError } from "./personSlice";
 import { fetchApiData } from "../../../fetchApiData";
-import {  selectPersonId } from "./personSlice";
 
-function* fetchPersonHandler() {
+function* fetchPersonHandler({payload: id}) {
     try {
-        const personId = yield select(selectPersonId);
-        const sourceApiData = `https://api.themoviedb.org/3/person/${personId}?api_key=6007bf485fd1645cfc7ab81654ba3228&language=en-US`
+        yield put(fetchPersonLoading());
+        yield delay(1000);
+        const sourceApiData = `https://api.themoviedb.org/3/person/${id}?api_key=6007bf485fd1645cfc7ab81654ba3228&language=en-US`
         const person = yield call(fetchApiData, sourceApiData);
         yield put(fetchPersonSuccess(person));
 
-        yield call(fetchFilmographyHandler, personId);
+        yield call(fetchFilmographyHandler, id);
 
     } catch (error) {
         yield put(fetchPersonError());
-
     }
 }
 
-function* fetchFilmographyHandler(personId) {
+function* fetchFilmographyHandler(id) {
     try {
-        const sourceApiData = `https://api.themoviedb.org/3/person/${personId}/movie_credits?api_key=6007bf485fd1645cfc7ab81654ba3228&language=en-US`
+        const sourceApiData = `https://api.themoviedb.org/3/person/${id}/movie_credits?api_key=6007bf485fd1645cfc7ab81654ba3228&language=en-US`
         const filmography = yield call(fetchApiData, sourceApiData);
         yield put(fetchFilmographySuccess(filmography));
     } catch (error) {
         yield put(fetchFilmographyError());
     }
 }
-
 
 export function* personSaga() {
     yield takeEvery(showPerson.type, fetchPersonHandler);
