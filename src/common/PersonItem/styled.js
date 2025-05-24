d1f854a6fc4770ae299f9d4a2031c991622da1c0
Adrian Markefka <am97veb@gmail.com>
2025-05-24 14:17:14 +0200

    changed element width, added media queries and removed unnecessary styles

--- src/common/PersonItem/styled.js
+++ src/common/PersonItem/styled.js
@@ -2,43 +2,54 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 export const StyledPersonItem = styled.li`
-    max-width: 255px;
+    max-width: 244px;
     width: 100%;
-    height: auto;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+    height: 100%;
     background-color: ${({theme})=> theme.colors.white};
     border-radius: 5px;
-    padding: 20px 20px;
-    box-shadow: 0px 4px 12px rgba(186, 199, 213, 0.5);
-    margin: 0px auto;
+    padding: 16px;
+    box-shadow: 0px 4px 12px ${({theme})=> theme.colors.shadowGrey};
+    list-style: none;
+    
+    @media (max-width: ${({ theme }) => theme.breakpoints.small}px) {
+        max-width: 170px;
+        padding: 8px;
+    }
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.extraSmall}px) {
+        max-width: 136px;
+    }
 `;
 
 export const StyledLink = styled(Link)`
     text-decoration: none;
     color: inherit;
-    width: 100%;
 `;
 
-export const Picture = styled.img`
-    width: 214px;
-    height: 321px;
+export const ImageWrapper = styled.div`
     width: 100%;
-    border-radius: 5px;
+    height: 321px;
+    display: flex;
     margin-bottom: 12px;
-    object-fit: cover;
-
+    
     @media (max-width: ${({ theme }) => theme.breakpoints.small}px) {
-        width: 138px;
-        height: 208px;
+        max-width: 154px;
+        max-height: 208px;
         margin-bottom: 8px;
     }
 
     @media (max-width: ${({ theme }) => theme.breakpoints.extraSmall}px) {
-        width: 120px;
-        height: 178px;
+        width: 118px;
+        height: 176px;
     }
+`;
+
+export const Picture = styled.img`
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    display: block;
+    border-radius: 5px;
 `;
 
 export const Name = styled.p`
@@ -46,7 +57,7 @@ export const Name = styled.p`
     font-weight: 500;
     line-height: 130%;
     text-align: center;
-    margin: 0px;
+    margin: 0 0 8px 0;
 
     @media (max-width: ${({ theme }) => theme.breakpoints.large}px) {       
         font-size: 20px;
@@ -61,18 +72,12 @@ export const Name = styled.p`
     }
 `;
 
-export const ImageWrapper = styled.div`
-    width: 100%;
-    display: flex;
-    justify-content: center;
-`;
-
 export const Role = styled.p`
     font-size: 18px;
     font-weight: 400;
     line-height: 150%;
     text-align: center;
-    margin: 8px 0 16px ;
+    margin: 0px;
     color: ${({ theme }) => theme.colors.lynch};
 
     @media (max-width: ${({ theme }) => theme.breakpoints.large}px) {       
