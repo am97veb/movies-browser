d87b589da32b8f431ce71f288b0fe391303a973f
Adrian Markefka <am97veb@gmail.com>
2025-05-24 14:20:30 +0200

    changed the width of the StyledEmptyPoster element, used colours from theme.js and improved code formatting

--- src/common/EmptyPoster/styled.js
+++ src/common/EmptyPoster/styled.js
@@ -11,11 +11,11 @@ export const StyledEmptyPoster = styled.div`
     align-items: center;
     border-radius: 5px;
     margin-bottom: 12px;
-    background-color:rgba(196, 196, 196, 1);
+    background-color:${({theme})=> theme.colors.posterGrey};
     object-fit: cover;
 
     @media (max-width: ${({ theme }) => theme.breakpoints.small}px) {
-        width: 138px;
+        width: 154px;
         height: 208px;
         margin-bottom: 8px;
     }
@@ -27,41 +27,40 @@ export const StyledEmptyPoster = styled.div`
 `;
 
 export const StyledEmptyPersonPoster = styled.div`
-        max-width: 399px;
-        max-height: 564px;
-        width: 25vw;
-        height: 35vw;
-        margin: 0px 40px 20px 0px;
-        float: left;
-        background-color:rgba(196, 196, 196, 1);
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        border-radius: 5px;
-        margin-bottom: 12px;
-        background-color:rgba(196, 196, 196, 1);
-        object-fit: cover;
+    max-width: 399px;
+    max-height: 564px;
+    width: 25vw;
+    height: 35vw;
+    margin: 0px 40px 20px 0px;
+    float: left;
+    background-color:${({theme})=> theme.colors.posterGrey};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 5px;
+    margin-bottom: 12px;
+    object-fit: cover;
 
-        @media (max-width: ${({ theme }) => theme.breakpoints.extraSmall}px) {
-            width: 116px;
-            height: 163px;
-        }
+    @media (max-width: ${({ theme }) => theme.breakpoints.extraSmall}px) {
+        width: 116px;
+        height: 163px;
+    }
 `;
 
 export const StyledEmptyProfilePath = styled(EmptyProfilePath)`
-    height:60px;
+    height: 60px;
 
     @media (min-width: ${({ theme }) => theme.breakpoints.extraExtraLarge}px) {
-        width:68px;
-        height:80px;
+        width: 68px;
+        height: 80px;
     }
 `;
 
 export const StyledEmptyPosterPath = styled(EmptyPosterPath)`
-    height:60px;
+    height: 60px;
 
     @media (min-width: ${({ theme }) => theme.breakpoints.extraExtraLarge}px) {
-        width:68px;
-        height:80px;
+        width: 68px;
+        height: 80px;
     }
 `;
