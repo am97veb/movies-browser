f9505b24721760e736a7b6390535bdebcb569be4
Adrian Markefka <am97veb@gmail.com>
2025-05-26 13:51:12 +0200

    separated moviesSaga - created a separate Saga and Slice file for downloading movie details

---
+++ src/features/movies/MoviePage/movieDetailsSaga.js
@@ -0,0 +1,34 @@
+import { put, call, takeEvery, delay, all } from "redux-saga/effects";
+import { fetchApiData } from "../../../fetchApiData";
+import {
+  getMovieDetails,
+  fetchMovieDetailsSuccess,
+  fetchMovieDetailsError,
+  fetchMovieDetailsLoading,
+} from "./movieDetailsSlice";
+
+export function* fetchMovieDetailsHandler({ payload: id }) {
+  try {
+    yield put(fetchMovieDetailsLoading());
+    yield delay(2000);
+    const detailsApiData = `https://api.themoviedb.org/3/movie/${id}?api_key=6007bf485fd1645cfc7ab81654ba3228&language=en-US`;
+    const creditsApiData = `https://api.themoviedb.org/3/movie/${id}/credits?api_key=6007bf485fd1645cfc7ab81654ba3228&language=en-US`;
+
+    const [movieDetails, credits] = yield all([
+      call(fetchApiData, detailsApiData),
+      call(fetchApiData, creditsApiData),
+    ]);
+
+    yield put(fetchMovieDetailsSuccess({
+      ...movieDetails,
+      cast: credits.cast,
+      crew: credits.crew,
+    }));
+  } catch (error) {
+    yield put(fetchMovieDetailsError());
+  }
+}
+
+export function* movieDetailsSaga() {
+  yield takeEvery(getMovieDetails.type, fetchMovieDetailsHandler);
+}
--- src/features/movies/moviesSlice.js
+++ src/features/movies/moviesSlice.js
@@ -5,64 +5,36 @@ const moviesSlice = createSlice({
   initialState: {
     moviesData: [],
     genres: [],
-    movieDetails: [],
     fetchMoviesStatus: "idle",
-    fetchMovieDetailsStatus: "idle",
   },
   reducers: {
     showMovies: () => { },
-    getMovieDetails: () => {
-    },
     fetchMoviesSuccess: (state, { payload: movies }) => {
       state.fetchMoviesStatus = "success";
       state.moviesData = movies.results;
     },
-    fetchMovieDetailsSuccess: (state, { payload }) => {
-      state.fetchMovieDetailsStatus = "success";
-      state.movieDetails = payload;
+    fetchMoviesLoading: (state) => {
+      state.fetchMoviesStatus = "loading";
     },
     fetchMoviesError: (state) => {
       state.fetchMoviesStatus = "error";
     },
-    fetchMovieDetailsError: (state) => {
-      state.fetchMovieDetailsStatus = "error";
-    },
     setGenres: (state, { payload: genres }) => {
       state.genres = genres;
-    },
-    fetchMovieDetailsLoading: (state) => {
-      state.fetchMovieDetailsStatus = "loading";
-    },
-    fetchMoviesLoading: (state) => {
-      state.fetchMoviesStatus = "loading";
-    },
-    clear: (state) => {
-      state.movieDetails = [];
-      state.movieId = "idle";
-      state.fetchMovieDetailsStatus = "idle";
     },
   },
 });
 
 export const {
   showMovies,
-  getMovieDetails,
   fetchMoviesSuccess,
-  fetchMovieDetailsSuccess,
   fetchMoviesError,
-  fetchMovieDetailsError,
   setGenres,
   fetchMoviesLoading,
-  fetchMovieDetailsLoading,
   clear
 } = moviesSlice.actions;
 export const selectMovies = state => state.movies.moviesData;
 export const selectGenres = state => state.movies.genres;
-export const selectMovieDetails = state => state.movies.movieDetails;
-export const selectCast = state => state.movies.movieDetails.cast;
-export const selectCrew = state => state.movies.movieDetails.crew;
 export const selectFetchMoviesStatus = state => state.movies.fetchMoviesStatus;
-export const selectFetchMovieDetailsStatus = state => state.movies.fetchMovieDetailsStatus;
 
 export default moviesSlice.reducer;
-
