88b5bbafbfec892683245fab0fbbd6ed922f3381
Adrian Markefka <am97veb@gmail.com>
2025-06-21 19:07:57 +0200

    simplified switch

--- src/common/SwitchContent/index.js
+++ src/common/SwitchContent/index.js
@@ -1,20 +1,13 @@
-import Error from "../Navigation/Search/Error";
-import Loading from "../Navigation/Search/Loading"
+import { Error } from "../Navigation/Search/Error";
+import { Loading } from "../Navigation/Search/Loading"
 
-export const SwitchContent = ({status, content}) => {
+export const SwitchContent = ({ status, content }) => {
     switch (status) {
-        case "idle":
-            return content;
-            
         case "loading":
             return <Loading />;
 
         case "error":
             return <Error />;
-
-        case "success":
-           return content;
-
         default:
             return content;
     }
--- src/features/people/People/index.js
+++ src/features/people/People/index.js
@@ -1,9 +1,9 @@
 import { useSelector } from "react-redux";
-import Loading from "../../../common/Navigation/Search/Loading";
-import Error from "../../../common/Navigation/Search/Error";
+import { Loading } from "../../../common/Navigation/Search/Loading";
+import { Error } from "../../../common/Navigation/Search/Error";
 import { PopularPeopleList } from "../PopularPeopleList";
 import { selectQuery, selectSearchResult, selectSearchStatus } from "../../../common/Navigation/Search/searchSlice";
-import NoResult from "../../../common/Navigation/Search/NoResult";
+import { NoResult } from "../../../common/Navigation/Search/NoResult";
 
 export const People = () => {
     const searchStatus = useSelector(selectSearchStatus);
@@ -11,8 +11,6 @@ export const People = () => {
     const searchResult = useSelector(selectSearchResult);
 
     switch (searchStatus) {
-        case "idle":
-            return <PopularPeopleList />
         case "error":
             return <Error />;
         case "loading":
