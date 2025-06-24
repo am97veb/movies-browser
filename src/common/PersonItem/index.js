7e52a59ac678179ff10c98cc14c01de443b2d12d
Adrian Markefka <am97veb@gmail.com>
2025-06-24 16:34:30 +0200

    removed unnecessery console.log

--- src/common/PersonItem/index.js
+++ src/common/PersonItem/index.js
@@ -2,28 +2,25 @@ import { StyledPersonItem, StyledLink, Picture, Name, ImageWrapper, Role } from 
 import { EmptyPoster } from "../EmptyPoster";
 import { toPersonDetails } from "../../core/routes";
 
-export const PersonItem = ({ id, image, name, character, department }) => {
-    console.log(id);
-    return (
-        <StyledPersonItem>
-            <StyledLink to={toPersonDetails(id)}>
-                <ImageWrapper>
-                    {image !== null ?
-                        <Picture
-                            src={`https://image.tmdb.org/t/p/w500${image}`}>
-                        </Picture>
-                        :
-                        <EmptyPoster />
-                    }
-                </ImageWrapper>
-                <Name>
-                    {name}
-                </Name>
-                    <Role>
-                        {character}
-                        {department}
-                    </Role>
-            </StyledLink>
-        </StyledPersonItem>
-    )
-};
+export const PersonItem = ({ id, image, name, character, department }) => (
+    <StyledPersonItem>
+        <StyledLink to={toPersonDetails(id)}>
+            <ImageWrapper>
+                {image !== null ?
+                    <Picture
+                        src={`https://image.tmdb.org/t/p/w500${image}`}>
+                    </Picture>
+                    :
+                    <EmptyPoster />
+                }
+            </ImageWrapper>
+            <Name>
+                {name}
+            </Name>
+            <Role>
+                {character}
+                {department}
+            </Role>
+        </StyledLink>
+    </StyledPersonItem>
+);
