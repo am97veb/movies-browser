faae98f101ef00f4395270f11809cd72639fa87e
Adrian Markefka <am97veb@gmail.com>
2025-06-26 23:35:12 +0200

    improved logic in fetchPaginationHandler

--- src/common/Pagination/paginationSaga.js
+++ src/common/Pagination/paginationSaga.js
@@ -4,17 +4,23 @@ import { selectContentType } from "./paginationSlice";
 import { fetchMoviesHandler } from "../../features/movies/moviesSaga";
 import { fetchPeopleHandler } from "../../features/people/peopleSaga";
 import { fetchSearchHandler } from "../Navigation/Search/searchSaga";
+import { fetchMovieDetailsHandler } from "../../features/movies/MoviePage/movieDetailsSaga";
+import { fetchPersonHandler } from "../../features/people/PersonPage/personSaga";
 
 function* fetchPaginationHandler() {
     try {
         const type = yield select(selectContentType);
         
-        if (type === null || type === "movies") {
+        if (type === "movies") {
             yield call(fetchMoviesHandler);
         } else if (type === "people") {
             yield call(fetchPeopleHandler);
-        } else if (type === "person" || type === "movie") {
-            yield call(fetchSearchHandler)
+        } else if (type === "searchedMovies" || type === "searchedPeople") {
+            yield call(fetchSearchHandler);
+        } else if (type === "movie") {
+            yield call(fetchMovieDetailsHandler);
+        } else if (type === "person" ) {
+            yield call(fetchPersonHandler);
         };
     } catch (error) {
         console.error('Error in pagination handler:', error);
@@ -22,6 +28,7 @@ function* fetchPaginationHandler() {
 }
 
 export function* paginationSaga() {
+    yield call(fetchPaginationHandler)
     yield takeEvery(
         [
             contentType.type,
