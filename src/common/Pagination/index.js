187d33500b6c9049048e5e7daa2e9766482bed58
Adrian Markefka <am97veb@gmail.com>
2025-06-26 23:34:27 +0200

    added new possible contentType to useEffect

--- src/common/Pagination/index.js
+++ src/common/Pagination/index.js
@@ -12,48 +12,72 @@ export const Pagination = () => {
    const currentContentType = useSelector(selectContentType);
 
    useEffect(() => {
-      const isMoviesPath = location.pathname.includes("movies") || location.pathname === "/";
-      const newContentType = isMoviesPath ? "movies" : "people";
+      const newContentType = (location.pathname.includes("movies") && location.search === "") || (location.pathname === "/" )
+      ?
+      "movies"
+      :
+      location.pathname.includes("people") && location.search === ""
+         ?
+         "people"
+         :
+         location.pathname.includes("movies") && location.search !== ""
+            ?
+            "searchedMovies"
+            :
+            location.pathname.includes("people") && location.search !== ""
+               ?
+               "searchedPeople"
+               :
+               location.pathname.includes(`movies/`)
+                  ?
+                  "movie"
+                  :
+                  location.pathname.includes(`people/`)
+                     ?
+                     "person"
+                     :
+                     null
+
 
       if (currentContentType !== newContentType) {
          dispatch(contentType(newContentType));
-      }
+      }    
 
-   }, [location.pathname, dispatch, currentContentType]);
-
+   }, [location.pathname, location, dispatch, currentContentType]);
+  
    return (
-      <StyledPagination>
-         <StyledList>
-            <Button disabled={page <= 1} >
-               <Link onClick={() => page > 1 && dispatch(firstPage())}>
-                  <MobileLeftArrow disabled={page <= 1} />
-                  <LeftArrow disabled={page <= 1} />
-                  First
-               </Link>
-            </Button>
-            <Button disabled={page <= 1}>
-               <Link onClick={() => page > 1 && dispatch(previousPage())}>
-                  <LeftArrow disabled={page <= 1} />
-                  Previous
-               </Link>
-            </Button>
-            <Text>
-               Page<Page>{page}</Page>of<Page>{totalPages}</Page>
-            </Text>
-            <Button disabled={page >= totalPages}>
-               <Link onClick={() => page < totalPages && dispatch(nextPage())}>
-                  Next
-                  <RightArrow disabled={page >= totalPages} />
-               </Link>
-            </Button>
-            <Button disabled={page >= totalPages}>
-               <Link onClick={() => page < totalPages && dispatch(lastPage())}>
-                  Last
-                  <RightArrow disabled={page >= totalPages} />
-                  <MobileRightArrow disabled={page >= totalPages} />
-               </Link>
-            </Button>
-         </StyledList>
-      </StyledPagination>
-   );
+   <StyledPagination>
+      <StyledList>
+         <Button disabled={page <= 1} >
+            <Link onClick={() => page > 1 && dispatch(firstPage())}>
+               <MobileLeftArrow disabled={page <= 1} />
+               <LeftArrow disabled={page <= 1} />
+               First
+            </Link>
+         </Button>
+         <Button disabled={page <= 1}>
+            <Link onClick={() => page > 1 && dispatch(previousPage())}>
+               <LeftArrow disabled={page <= 1} />
+               Previous
+            </Link>
+         </Button>
+         <Text>
+            Page<Page>{page}</Page>of<Page>{totalPages}</Page>
+         </Text>
+         <Button disabled={page >= totalPages}>
+            <Link onClick={() => page < totalPages && dispatch(nextPage())}>
+               Next
+               <RightArrow disabled={page >= totalPages} />
+            </Link>
+         </Button>
+         <Button disabled={page >= totalPages}>
+            <Link onClick={() => page < totalPages && dispatch(lastPage())}>
+               Last
+               <RightArrow disabled={page >= totalPages} />
+               <MobileRightArrow disabled={page >= totalPages} />
+            </Link>
+         </Button>
+      </StyledList>
+   </StyledPagination>
+);
 };
