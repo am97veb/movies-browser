1d7effd5dfbb315d2c6db97e6ef317bd517955f7
Adrian Markefka <am97veb@gmail.com>
2025-06-28 17:32:34 +0200

    the pagination action has been integrated into the URL and fetchStatus added

--- src/features/people/PopularPeopleList/index.js
+++ src/features/people/PopularPeopleList/index.js
@@ -1,20 +1,29 @@
 import { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { PersonItem } from "../../../common/PersonItem/index";
 import { Wrapper, Header, PeopleList } from "./styled";
-import { selectPeople, selectPeopleStatus } from "../peopleSlice";
+import { clear, selectPeople, selectPeopleStatus } from "../peopleSlice";
 import { Pagination } from "../../../common/Pagination";
-import { useQueryParameters } from "../../../common/Navigation/Search/queryParameters";
+import { useQueryParameters, useReplaceQueryParameter } from "../../../common/Navigation/Search/queryParameters";
 import searchQueryParamName from "../../../common/Navigation/Search/searchQueryParamName";
-import { selectSearchResult, selectQuery } from "../../../common/Navigation/Search/searchSlice";
-import { selectPage } from "../../../common/Pagination/paginationSlice";
+import { selectSearchResult, selectQuery, selectSearchStatus } from "../../../common/Navigation/Search/searchSlice";
+import { selectContentType, selectPage, setPage } from "../../../common/Pagination/paginationSlice";
 import { SwitchContent } from "../../../common/SwitchContent";
+import { useClearData } from "../../../common/useClearData";
 
-export const PopularPeopleList = () => {
-   const query = useQueryParameters(searchQueryParamName);
+export const PopularPeopleList = ({ searchTerm }) => {
+
    const sliceQuery = useSelector(selectQuery);
    const page = useSelector(selectPage);
-   const fetchStatus = useSelector(selectPeopleStatus);
+   const dispatch = useDispatch();
+   const query = useQueryParameters(searchQueryParamName);
+   const setUrl = useReplaceQueryParameter();
+   const pageFromUrl = useQueryParameters("page");
+
+   const fetchStatus = useSelector(state =>
+      (query?.trim() && query === sliceQuery)
+         ? selectSearchStatus(state)
+         : selectPeopleStatus(state));
 
    const people = useSelector(state =>
       (query?.trim() && query === sliceQuery)
@@ -22,31 +31,46 @@ export const PopularPeopleList = () => {
          : selectPeople(state));
 
    useEffect(() => {
+      const pageFromUrlToNumber = pageFromUrl ? parseInt(pageFromUrl) : 1;
+
+      if (page !== pageFromUrlToNumber) {
+         dispatch(setPage(pageFromUrlToNumber));
+      }
+   }, [pageFromUrl, dispatch])
+
+   useEffect(() => {
       window.scrollTo(0, 0);
+      page > 1 &&
+         setUrl({
+            key: "page",
+            value: page,
+         });
    }, [page])
+
+   useClearData({ clear });
 
    return (
       <SwitchContent
          status={fetchStatus}
          content={
-               <Wrapper>
-                  <Header>
-                     Popular people
-                  </Header>
-                  <PeopleList>
-                     {Array.isArray(people) &&
-                        people.map(person => (
-                           <PersonItem
-                              key={person.id}
-                              id={person.id}
-                              image={person.profile_path}
-                              name={person.name}
-                           />
-                        ))
-                     }
-                  </PeopleList>
-                  <Pagination />
-               </Wrapper>
+            <Wrapper>
+               <Header>
+                  {searchTerm || "Popular People"}
+               </Header>
+               <PeopleList>
+                  {Array.isArray(people) &&
+                     people.map(person => (
+                        <PersonItem
+                           key={person.id}
+                           id={person.id}
+                           image={person.profile_path}
+                           name={person.name}
+                        />
+                     ))
+                  }
+               </PeopleList>
+               <Pagination />
+            </Wrapper>
          }
       />
    )
