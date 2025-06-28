7d91ae8d4d992adf666168bb0197eb5c3c19b5f6
Adrian Markefka <am97veb@gmail.com>
2025-06-28 17:34:54 +0200

    initial contentType was set to "movies" and reducer setPage was added

--- src/common/Pagination/paginationSlice.js
+++ src/common/Pagination/paginationSlice.js
@@ -5,15 +5,18 @@ const paginationSlice = createSlice({
     initialState: {
         page: 1,
         totalPages: 1,
-        contentType: null,
+        contentType: "movies",
     },
     reducers: {
         contentType: (state, {payload: contentType}) => {
             state.contentType = contentType;
             state.page = 1;
         },
+        setPage: (state, {payload: page}) => {
+            state.page = page;
+        },
         totalPages:(state, {payload: data}) =>{
-            state.totalPages = data.total_pages > 500 ? 500 : data.total_pages;
+            state.totalPages = data.total_pages < 500 ? data.total_pages : 500;
         },
         firstPage: (state) => {
             state.page = 1
@@ -30,7 +33,7 @@ const paginationSlice = createSlice({
     },
 });
 
-export const { contentType, totalPages, firstPage, nextPage, previousPage, lastPage } = paginationSlice.actions;
+export const { contentType, setPage, totalPages, firstPage, nextPage, previousPage, lastPage } = paginationSlice.actions;
 export const selectPage = state => state.pagination.page;
 export const selectTotalPages = state => state.pagination.totalPages;
 export const selectContentType = state => state.pagination.contentType;
